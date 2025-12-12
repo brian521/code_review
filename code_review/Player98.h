06110e1015bcda96d24cbfe2992f7b807ba63774
OptimalTime99 <optimal9744@gmail.com>
2025-12-12 20:23:30 +0900

    [Create/OptimalTime99] Player98.h

---
+++ code_review/Player98.h
@@ -0,0 +1,13 @@
+// copyright 2025 OptimalTime99 all rights reserved.
+
+#pragma once
+
+class Player98 {
+private:
+    int hp;
+    int mp;
+public:
+    Player98();
+    int getHP();
+    void setHP(int hp);
+};
